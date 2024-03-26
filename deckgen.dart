8a052734911449f63ec6cdb3c39d4106d1365c63
CodyKoInABox <codykoinabox@gmail.com>
2024-03-26 00:31:46 -0300

    add suit value to Card class

--- deckgen.dart
+++ deckgen.dart
@@ -6,15 +6,16 @@ class Card{
   late String value;
   late String suit;
   late int trueValue;
+  late int suitValue;
 
-  Card(this.value, this.suit, this.trueValue);
+  Card(this.value, this.suit, this.trueValue, this.suitValue);
 
   @override
-  String toString() => "[($trueValue) $value of $suit]";
+  String toString() => "[($trueValue) $value of ($suitValue)$suit]";
 }
 
 void main(){
-  Card card = new Card('10', 'Spades', 10);
+  Card card = new Card('10', 'Spades', 10, 3);
 
   print(card);
 }
